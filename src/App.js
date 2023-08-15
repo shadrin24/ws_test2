12791d3b2720e3d406cfeb78cdca4f0f06bbc433
shadrin24 <shadrin24@bk.ru>
2023-08-15 22:37:39 +0700

    https://youtu.be/GNrdg3PzpJQ?t=2062

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
+import './styles/App.css';
 
 function App() {
+    const [value, setValue] = useState('ТЕКСТ В ИНПУТЕ')
+
   return (
     <div className="App">
-
+        <div className='post'>
+            <div className='post__content'>
+                <strong>1. Javascript</strong>
+                <div>
+                    Javascript - язык программирования
+                </div>
+            </div>
+            <div className="post__btns">
+                <button>Удалить</button>
+            </div>
+        </div>
     </div>
   );
 }
