a9c1310c584a71a6b91884f16035773e359d67eb
shadrin24 <shadrin24@bk.ru>
2023-08-16 20:07:21 +0700

    https://youtu.be/GNrdg3PzpJQ?t=2934

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import ClassCounter from "./components/ClassCounter";
 import './styles/App.css';
 import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -12,11 +14,19 @@ function App() {
         {id: 3, title: 'Javascriprt 3', body: 'Description'}
     ])
 
-  return (
-    <div className="App">
-        <PostList posts={posts} title={'Посты про JS'}/>
-    </div>
-  );
+    const addNewPost = () => {
+
+    }
+
+    return (
+        <div className="App">
+            <form></form>
+            <MyInput type="text" placeholder="Название поста"/>
+            <MyInput type="text" placeholder="Описание поста"/>
+            <MyButton>Создать пост</MyButton>
+            <PostList posts={posts} title={'Посты про JS'}/>
+        </div>
+    );
 }
 
 export default App;
