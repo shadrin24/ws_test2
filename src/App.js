d7a9f693dc669076b4cf57b1746f951e97eb3007
i.shadrin <i.shadrin.sis@gmail.com>
2023-08-16 08:03:35 +0700

    https://youtu.be/GNrdg3PzpJQ?t=2525

--- src/App.js
+++ src/App.js
@@ -2,23 +2,19 @@ import React, {useState} from 'react';
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import './styles/App.css';
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 
 function App() {
-    const [value, setValue] = useState('ТЕКСТ В ИНПУТЕ')
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'Javascriprt', body: 'Description'},
+        {id: 2, title: 'Javascriprt 2', body: 'Description'},
+        {id: 3, title: 'Javascriprt 3', body: 'Description'}
+    ])
 
   return (
     <div className="App">
-        <div className='post'>
-            <div className='post__content'>
-                <strong>1. Javascript</strong>
-                <div>
-                    Javascript - язык программирования
-                </div>
-            </div>
-            <div className="post__btns">
-                <button>Удалить</button>
-            </div>
-        </div>
+        <PostList posts={posts} title={'Посты про JS'}/>
     </div>
   );
 }
